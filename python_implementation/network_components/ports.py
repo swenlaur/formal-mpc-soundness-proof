6f572a8b18f9bf18bf4558ed940c522c22b108f8
swenlaur <swen@ut.ee>
2022-03-03 00:39:25 +0200

    Relaxed the type of port labels

--- python_implementation/network_components/ports.py
+++ python_implementation/network_components/ports.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass
 from network_components import Machine
 
+from typing import Any
+
 
 @dataclass
 class InputPort:
     machine: Machine
-    port_number: int
+    port_label: Any
 
 
 @dataclass
 class OutputPort:
     machine: Machine
-    port_number: int
+    port_label: Any
