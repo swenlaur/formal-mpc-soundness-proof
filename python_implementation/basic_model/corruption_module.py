f78a0c2f4e6b3d3e7760cc7d310fe6f63ab56010
swenlaur <swen@ut.ee>
2022-03-05 23:22:10 +0200

    Corruption module is complete

--- python_implementation/basic_model/corruption_module.py
+++ python_implementation/basic_model/corruption_module.py
@@ -11,7 +11,7 @@ from typing import Optional
 class CorruptionModule(Machine):
     """
     Corruption module controls how the interpreter interacts with outside world.
-    The module sends messages only to outgoing buffers or instantly to the adversary.
+    The module writes messages only to outgoing buffers or send them instantly to the adversary.
     """
 
     def __init__(self, interpreter: StatefulInterpreter):
@@ -23,8 +23,8 @@ class CorruptionModule(Machine):
         """
         Processes inputs from incoming buffers.
         When the party is honest invokes the interpreter and stops without output.
-        When the party is corrupted returns the input to the adversary.
-        In both cases the output formally goes to the adversary.
+        When the party is corrupted returns the input (input port and message) to the adversary.
+        In both cases the output formally goes to the adversary but this is empty when the party is honest.
         """
         if self.corrupted:
             return input_port, msg
@@ -42,14 +42,16 @@ class CorruptionModule(Machine):
         assert 0 <= input_port < len(self.outgoing_buffers)
         return self.outgoing_buffers[input_port].write_message(msg)
 
-    def write_to_interpreter(self, input_port: int, msg: Any) -> Optional[Tuple[int, Any]]:
+    def write_to_interpreter(self, input_port: int, msg: Any) -> List[Tuple[int, Any]]:
         """
         Adversary can send a message to the interpreter when the party is corrupted.
         The input port determines how the interpreter treats the input:
         0     -- write as an adversary
         1...k -- write as an ideal functionality
         k + 1 -- write as an parent party from the environment
-        BUG: Wrong output type
+
+        Returns a list of port labels and corresponding messages the interpreter has decided to write into
+        outgoing buffers. As the party is corrupted the adversary must decide what to do with this further.
         """
         assert self.corrupted
         return self.interpreter(input_port, msg)
