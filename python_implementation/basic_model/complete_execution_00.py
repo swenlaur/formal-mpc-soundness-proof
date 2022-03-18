51a9fd33bdac8555b0e14753c83a21b53712ea63
swenlaur <swen@ut.ee>
2022-03-18 23:59:35 +0200

    Main execution loop is now correct

--- python_implementation/basic_model/complete_execution_00.py
+++ python_implementation/basic_model/complete_execution_00.py
@@ -17,9 +17,7 @@ from adversarial_actions import SendOutgoingMessage
 from adversarial_actions import QueryFunctionality
 from adversarial_actions import InvokeEnvironment
 
-
 from typing import Dict
-from typing import Tuple
 from typing import List
 
 n: int = 2
@@ -29,54 +27,42 @@ k: int = 2
 parameter_set = trusted_setup()
 protocol_description = protocol_description()
 
-# Set up environment
 # noinspection PyTypeChecker
 parent_parties: List[ParentParty] = [None] * n
-for i, pk, sk in enumerate(parameter_set[:n]):
-    parent_parties[i] = ParentParty(pk, sk)
-environment = Environment(parent_parties)
-
-# Initialise protocol parties
 # noinspection PyTypeChecker
-corruption_modules: List[ProtocolParty] = [None] * n
-for i, pk, sk in enumerate(parameter_set[:n]):
-    corruption_modules[i] = ProtocolParty(pk, sk, protocol_description[i], k + 1)
-
+protocol_party: List[ProtocolParty] = [None] * n
 # noinspection PyTypeChecker
 ideal_functionalities: List[StandardFunctionality] = [None] * k
+
+# Set up environment and protocol parties
+for i, pk, sk in enumerate(parameter_set[:n]):
+    parent_parties[i] = ParentParty(pk, sk)
+    protocol_party[i] = ProtocolParty(pk, sk, protocol_description[i], k + 1)
+environment = Environment(parent_parties)
+
+# Set up ideal functionalities
 for i, pk, sk in enumerate(parameter_set[n:n+k]):
     ideal_functionalities[i] = StandardFunctionality(pk, sk)
 
-# Initialise protocol wiring. Buffers are indexed with integers for universality
-incoming_buffers: Dict[Tuple[int, int], LeakyBuffer] = {}
-outgoing_buffers: Dict[Tuple[int, int], LeakyBuffer] = {}
-for i, p in enumerate(corruption_modules):
-    # noinspection PyTypeChecker
-    for j, f in enumerate(ideal_functionalities + [environment]):
-        incoming_buffers[i, j] = LeakyBuffer()
-        outgoing_buffers[i, j] = LeakyBuffer()
-
-# Complete setup by specifying outgoing buffers
-# for i, corruption_module in enumerate(corruption_modules):
-#    corruption_module.set_outgoing_buffers([outgoing_buffers[i, j] for j in range(k + 1)])
-
-for j, functionality in enumerate(ideal_functionalities):
-    functionality.set_outgoing_buffers([incoming_buffers[i, j] for i in range(n)])
-environment.set_outgoing_buffers([incoming_buffers[i, k+1] for i in range(n)])
-
 # Set up the adversary
 public_param, private_param = parameter_set[n + k]
-adversary = LazyAdversary(public_param, private_param, environment)
+adversary = LazyAdversary(public_param, private_param, environment)  # TODO: remove env from the adversary
+
+# Initialise protocol wiring. The first index marks protocol party. The second index marks ideal functionality.
+incoming_buffers: Dict[int, Dict[int, LeakyBuffer]] = {i: {j: LeakyBuffer() for j in range(k + 1)} for j in range(n)}
+outgoing_buffers: Dict[int, Dict[int, LeakyBuffer]] = {i: {j: LeakyBuffer() for j in range(k + 1)} for j in range(n)}
 
 # Complete execution
 action = adversary.next_action(None)
 while action is not None:
     if isinstance(action, CorruptParty):
-        reply = corruption_modules[action.party].corrupt_party()
+        reply = protocol_party[action.party].corrupt_party()
         action = adversary.next_action(reply)
     elif isinstance(action, ClockIncomingBuffer):
         msg = incoming_buffers[action.target, action.source].clock_message(action.msg_index)
-        reply = corruption_modules[action.target](action.source, msg)
+        write_instructions, reply = protocol_party[action.target](action.source, msg)
+        for port, msg in write_instructions:
+            outgoing_buffers[action.target][port].write_message(msg)
         action = adversary.next_action(reply)
     elif isinstance(action, ClockOutgoingBuffer):
         msg = outgoing_buffers[action.source, action.target].clock_message(action.msg_index)
@@ -86,10 +72,14 @@ while action is not None:
             environment(action.source, msg)
         action = adversary.next_action(None)
     elif isinstance(action, SendIncomingMessage):
-        reply = corruption_modules[action.target].write_to_interpreter(action.source, action.msg)
+        write_instructions, reply = protocol_party[action.target].write_to_interpreter(action.source, action.msg)
+        for port, msg in write_instructions:
+            outgoing_buffers[action.target][port].write_message(msg)
         action = adversary.next_action(reply)
     elif isinstance(action, SendOutgoingMessage):
-        corruption_modules[action.source].write_to_outgoing_buffer(action.target, action.msg)
+        write_instructions = protocol_party[action.source].write_to_outgoing_buffer(action.target, action.msg)
+        for port, msg in write_instructions:
+            outgoing_buffers[action.target][port].write_message(msg)
         action = adversary.next_action(None)
     elif isinstance(action, InvokeEnvironment):
         reply = environment.adversarial_probe(action.msg)
