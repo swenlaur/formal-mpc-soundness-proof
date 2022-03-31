ff734abef423ea70b297ee930ea573a1290bd950
johanna maria <100830871+rho-dodendron@users.noreply.github.com>
2022-03-31 09:40:13 +0300

    incorporated feedback from sven & dominique
    updated bufferAction to be as Sven suggested, which also satisfied Dominique wanting to collapse the 2-3-field records. next: figuring out instanceState, and moving on to other folders

--- isabelle_proofs/AdversarialAction.thy
+++ isabelle_proofs/AdversarialAction.thy
@@ -1,14 +1,6 @@
-(* Sent this and DataTypes to Dominique as well, who will correct/feedback this. *)
-
 theory AdversarialAction
   imports Main DataTypes
 begin
-
-(*Sven said we should synchronize our nomenclature, but I'm not sure how to do that exactly,
-but I'll aim to be really clear, at least. *)
-
-(* Sven has a separate "datatypes" folder. That doesn't exactly jive here? *)
-(* The Datatypes are quite interesting, but also a lot of them are empty?*)
 
 (* Swen:
    From technical viewpoint we can have only one data type for clock buffer
@@ -28,57 +20,31 @@ record bufferAction =
 
   source and target fields are bad names since these would be in reverse
   for incoming and outcoming actions
-  
+ 
 *)
 
-record clockBuffer =
-  bufferSource :: partyID
-  bufferTarget :: functionalityID
-  bufferMsgIndex :: messageIndex
+datatype dir = Incoming | Outgoing
+datatype act = Clock | Peek
 
-record corruptParty =
-  corruptPartyID :: partyID
-
-record invokeEnvironment =
-  invokeMessage :: message
-
-record  sendMessage =
-  sendSource :: nat
-  sendTarget :: nat
-  sendMessage :: message
-
-(*Here's the thing here: sendSource behaves as partyID or portNumber,
-depending on whether we're sending an incoming or outgoing message.
-
-So it's either like this (generic), or I'll split it as follows:
-
-record sendIncomingMessage =
-  sendISource :: portNumber
-  sendITarget :: partyID
-  sendIMessage :: message
-
-record sendOutgoingMessage =
-  sendOSource :: partyID
-  sendOTarget :: portNumber
-  sendOMessage :: message *)
+record bufferAction =
+  party :: partyID
+  functionality :: functionalityID
+  direction :: dir
+  action :: act
+  msgIndex :: messageIndex
 
 
-record queryFunctionality =
+record queryFunctionality = 
   queryTarget :: functionalityID
   queryModule :: moduleType
-  queryInstance :: string
- (* instanceLabel (* What kind of a thing should instanceLabel be?  *)*)
+  queryInstance :: instanceLabel
   queryMessage :: message
 
 
 datatype adversarialAction =
-  ClockIncomingBuffer clockBuffer |
-  ClockOutgoingBuffer clockBuffer |
-  CorruptParty corruptParty |
-  SendOutgoingMessage sendMessage |
-  SendIncomingMessage sendMessage |
-  InvokeEnvironment invokeEnvironment |
+  BufferAction bufferAction |
+  CorruptParty partyID |
+  InvokeEnvironment message |
   QueryFunctionality queryFunctionality
-(* ? *)
 
 end
